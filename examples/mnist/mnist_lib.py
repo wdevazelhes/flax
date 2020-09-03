a4011cb692181edd00889ec6f4691878815e2433
Automated Publisher <actions@users.noreply.github.com>
2020-09-03 08:13:10 +0000

    Added howto branch howto/checkpointing

--- examples/mnist/mnist_lib.py
+++ examples/mnist/mnist_lib.py
@@ -32,6 +32,7 @@ import tensorflow_datasets as tfds
 from flax import nn
 from flax import optim
 from flax.metrics import tensorboard
+from flax.training import checkpoints
 
 class CNN(nn.Module):
   """A simple CNN model."""
@@ -166,6 +167,9 @@ def train_and_evaluate(model_dir: str, num_epochs: int, batch_size: int,
   model = create_model(init_rng)
   optimizer = create_optimizer(model, learning_rate, momentum)
 
+  # Restore saved model
+  optimizer = checkpoints.restore_checkpoint(model_dir, optimizer)
+
   for epoch in range(1, num_epochs + 1):
     rng, input_rng = random.split(rng)
     optimizer, train_metrics = train_epoch(
@@ -179,6 +183,9 @@ def train_and_evaluate(model_dir: str, num_epochs: int, batch_size: int,
     summary_writer.scalar('train_accuracy', train_metrics['accuracy'], epoch)
     summary_writer.scalar('eval_loss', loss, epoch)
     summary_writer.scalar('eval_accuracy', accuracy, epoch)
+
+    # Save model at every epoch
+    checkpoints.save_checkpoint(model_dir, optimizer, epoch, keep=3)
 
   summary_writer.flush()
   return optimizer
